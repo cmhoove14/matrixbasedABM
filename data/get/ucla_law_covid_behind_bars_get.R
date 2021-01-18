6c5a81b7186027eb3d99ce301dbdcc2c5331ff97
cmhoove14 <cmhoove14@gmail.com>
2021-01-17 18:47:40 -0800

    Updates to data

--- data/get/ucla_law_covid_behind_bars_get.R
+++ data/get/ucla_law_covid_behind_bars_get.R
@@ -1,5 +1,5 @@
 # Get data from UCLA Covid Behind Bars github project https://github.com/uclalawcovid19behindbars
 
-uclabb <- readr::read_csv(url("https://raw.githubusercontent.com/uclalawcovid19behindbars/historical-data/main/data/CA_adult_facility_covid_counts_historical.csv"))
+uclabb <- readr::read_csv(url("https://raw.githubusercontent.com/uclalawcovid19behindbars/historical-data/main/data/CA-historical-data.csv"))
 
 saveRDS(uclabb, "data/raw/ucla_law_covid_behind_bars_github_data_1-11-21.rds")
