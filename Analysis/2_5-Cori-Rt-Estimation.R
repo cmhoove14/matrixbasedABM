30c52553ef4b31d016f7e6c98c0460fdb5efb15f
cmhoove14 <cmhoove14@gmail.com>
2021-01-17 18:47:21 -0800

    set seed and save

--- Analysis/2_5-Cori-Rt-Estimation.R
+++ Analysis/2_5-Cori-Rt-Estimation.R
@@ -45,7 +45,8 @@ cori_rt <- function(facil, dat){
   R_config <- EpiEstim::make_config(t_start = ts,
                                     t_end = te,
                                     mean_si = 5.2, # From https://dx.doi.org/10.1016%2Fj.cegh.2020.08.007
-                                    std_si = 1)
+                                    std_si = 1,
+                                    seed = 430)
   
   Cori_R <- EpiEstim::estimate_R(incid = inc_dat,
                                  method = "parametric_si",
@@ -63,31 +64,5 @@ cori_rts <- bind_rows(lapply(unique(dat$Facility),
                              FUN = cori_rt,  
                              dat = dat))
 
-
-
-
-
-SF_cori_R$R %>%
-  mutate(Date = as.Date("2020-03-05")+t_start) %>% 
-  ggplot() +
-  theme_bw() +
-  geom_col(data = sf_case,
-           aes(x = Date, y = (Cases/SF.pop)*1e5),
-           fill = "blue", col = "darkblue", alpha = 0.3) +
-  geom_line(aes(x = Date, y = `Median(R)`),
-            size = 1.2, col = "red") +
-  geom_ribbon(aes(x = Date, y = `Median(R)`,
-                  ymin = `Quantile.0.025(R)`,
-                  ymax = `Quantile.0.975(R)`),
-              fill = "red", alpha = 0.4) +
-  geom_hline(yintercept = 1, lty = 2) +
-  scale_x_date(date_labels = "%m/%d", 
-               date_breaks = "7 day") +
-  theme(axis.title = element_text(size = 14,
-                                  face = "bold"),
-        axis.text = element_text(size = 12),
-        axis.text.x = element_text(angle = 45,
-                                   hjust = 1)) +
-  labs(x = "Date",
-       y = expression(paste(R[e], " estimate/cases reported per 100k SF residents")),
-       title = "Cori et al estimate of R through time from SF testing data")
+saveRDS(cori_rts, 
+        here::here("data", "derived", "Cori_etal_Rts.rds"))
