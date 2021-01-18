e4130ca078fc659dedfe1640304dc9a2ede2bf03
cmhoove14 <cmhoove14@gmail.com>
2021-01-18 15:24:47 -0800

    Updated data cleaning and resulting dataset

--- Analysis/0-data-load-clean.R
+++ Analysis/0-data-load-clean.R
@@ -6,11 +6,37 @@
 library(zoo)
 library(tidyverse)
 
+# Functions (to be moved to R package infrastructure)
+
+# Function for initial estimate of incidence from change in cumulative, then replace negative changes with NAs
+change_rmv_nas <- function(vec){
+  # Find values that result in negative changes and replace with NA
+  diff_vec <- c(NA, diff(vec))
+  vec2 <- vec
+  vec2[is.na(vec)] <- NA
+  vec2[diff_vec < 0] <- NA
+  
+  return(vec2)
+}
+
+
+# Function to return difference of cumsum vectors removing NAs
+cumsum_diffs <- function(vec1, vec2, vec3){
+  vec1[is.na(vec1)] <- 0
+  vec2[is.na(vec2)] <- 0
+  vec3[is.na(vec3)] <- 0
+  
+  out <- cumsum(vec1) - cumsum(vec2) - cumsum(vec3)
+  
+  return(out)
+}
+
 # Load Case and population data -------------------
 
 # See data/get for scripts to get and process data from online sources
 
-uclabb <- readRDS(here::here("data", "raw", "ucla_law_covid_behind_bars_github_data_1-11-21.rds"))
+uclabb <- readRDS(here::here("data", "raw", "ucla_law_covid_behind_bars_github_data_1-17-21.rds")) %>% 
+  dplyr::select(-(Is.Different.Operator:Source.Capacity))
 pop_sps <- readRDS(here::here("data", "derived", "cdcr_population_ts_2020-01-11.rds")) %>% 
   mutate(dplyr::across(.cols = c("Capacity", "Design_Capacity", "Percent_Occupied", "Staffed_Capacity"),
                 .fns = as.numeric))
@@ -22,32 +48,30 @@ case_facs <- unique(uclabb$Name)
 facility_lookup_table <- data.frame(Facility_pop = pop_facs,
                                     Facility_case = NA)
 
-# Match 3/4 letter codes for facilities
-for(i in 1:length(facility_lookup_table$Facility_pop)){
-  fac_code <- gsub(".*\\((.*)\\).*", "\\1", facility_lookup_table$Facility_pop[i])
-  
-  init_grep <- grepl(fac_code, case_facs)
-  
-  if(sum(init_grep) == 1){
-    fac_case <- case_facs[which(init_grep)]
-  } else {
-    fac_case <- sum(init_grep)
+# Match facility names
+facility_lookup_table <- bind_rows(lapply(
+  case_facs, function(f){
+    init_grep <- grepl(f, pop_facs, ignore.case = T)
+    
+    return(list("Facility_pop" = pop_facs[which(init_grep)],
+                "Facility_case" = f))
   }
-  
-  facility_lookup_table$Facility_case[i] <- fac_case
-}
+))
 
-# Above works for most, but some require manual assignment
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "Calipatria State Prison (CAL)")] <- "CAL CALIPATRIA STATE PRISON"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "Centinela State Prison (CEN)")] <- "CEN CALIFORNIA STATE PRISON CENTINELA"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California Health Care Facility - Stockton (CHCF)")] <- "CALIFORNIA HEALTH CARE FACILITY CHCF STOCKTON"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California State Prison Corcoran (COR)")] <- "COR CALIFORNIA STATE PRISON CORCORAN"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California Rehabilitation Center (CRC)")] <- "CRC CALIFORNIA REHABILITATION CENTER"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "Ironwood State Prison (ISP)")] <- "ISP IRONWOOD STATE PRISON"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California State Prison Sacramento (SAC)")] <- "SAC CALIFORNIA STATE PRISON SACRAMENTO"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California Substance Abuse Treatment Facility (SATF)")] <- "SATF CSP CALIFORNIA SUBSTANCE ABUSE TREATMENT FACILITY AND STATE PRISON CORCORAN"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "California State Prison Solano (SOL)")] <- "SOL CALIFORNIA STATE PRISON SOLANO"
-facility_lookup_table$Facility_case[which(facility_lookup_table$Facility_pop == "Valley State Prison (VSP)")] <- "VSP VALLEY STATE PRISON"
+# Remove duplicates that arise due to partial matching
+facility_lookup_table <- facility_lookup_table[!duplicated(facility_lookup_table$Facility_pop),]
+
+#Identify facilities that got left behind and append to lookup table
+missed_facs <- pop_facs[!pop_facs %in% facility_lookup_table$Facility_pop]
+
+facility_lookup_table <- rbind(facility_lookup_table,
+                               data.frame(Facility_pop = missed_facs, 
+                                          Facility_case = c("CALIFORNIA MENS COLONY",
+                                                            "CALIFORNIA STATE PRISON SAN QUENTIN",
+                                                            "CENTRAL CALIFORNIA WOMENS FACILITY")))
+
+# Should be 0 if all matched
+pop_facs[!pop_facs %in% facility_lookup_table$Facility_pop]
 
 # Merge datasets -----------------
 #Folsom State Prison has both male and female populationsin the population dataset, but the cases are reported facility wide. Unclear whether the case reporting is for both facilities or only for the male facility, but will assume they're facility-wide and therefore just merge the population data for Folsom
@@ -68,97 +92,97 @@ pop_case_merge <- uclabb %>%
   left_join(pop_sps2, by = c("Facility_pop" = "Facility",
                              "Date" = "Report_Date"))
 
-# Function to clean, interpolate, and smooth vector of values. Used below to convert cumulative counts into incident counts, skipping over data errors that present as negative changes and then smoothing 
-clean_interpolate <- function(vec){
-  # Find values that result in negative changes and replace with NA
-  diff_vec <- c(NA, diff(vec))
-  vec2 <- vec
-  vec2[is.na(vec)] <- NA
-  vec2[diff_vec < 0] <- NA
-  
-  # Fill in NAs via interpolation then get new differences and smooth
-  vec_interp <- na.approx(vec2, na.rm = FALSE)
-  
-  return(vec_interp)
-}
-
-# final processing to drop superfluous columns, interpolate population, derive smoothed and corrected case incidence
-fin_dat <- pop_case_merge %>%
+intermediate_clean <- pop_case_merge %>%
   rename(Facility = Facility_pop) %>%
   filter(!is.na(Facility)) %>% 
   group_by(Facility) %>%
   padr::pad() %>%
+# Add a few columns   
   mutate(
     Resident_Outbreak_Start = min(Date[which(Residents.Confirmed > 0)], na.rm = T),
-    Resident_Outbreak_Day = as.numeric(Date - Resident_Outbreak_Start),
-    Staff_Outbreak_Start = min(Date[which(Staff.Confirmed > 0)], na.rm = T),
-    Staff_Outbreak_Day = as.numeric(Date - Staff_Outbreak_Start),
-    Pop_interpolated        = na.approx(Capacity, na.rm = FALSE),
-    Residents_Confirmed2    = clean_interpolate(Residents.Confirmed),
-    New_Resident_Cases = Residents_Confirmed2 - lag(Residents_Confirmed2),
-    New_Resident_Cases_7day = zoo::rollapply(data = Residents_Confirmed2 - lag(Residents_Confirmed2), 
-                                             width = 7,
-                                             FUN = mean,
-                                             na.rm = T,
-                                             fill = NA,
-                                             align = "right"),
-    Residents_Recovered2 = clean_interpolate(Residents.Recovered),
-    Residents_Recovered_7day = zoo::rollapply(na.approx(Residents.Recovered, na.rm = F),
-                                             width = 7,
-                                             FUN = mean,
-                                             na.rm = T,
-                                             fill = NA,
-                                             align = "right"),
-    Residents_Active_7day = zoo::rollapply(Residents_Confirmed2 - Residents_Recovered2,
-                                           width = 7,
-                                           FUN = mean,
-                                           na.rm = T,
-                                           fill = NA,
-                                           align = "right"),
-    Residents_Deaths_7day = zoo::rollapply(na.approx(Residents.Deaths, na.rm = F),
-                                           width = 7,
-                                           FUN = mean,
-                                           na.rm = T,
-                                           fill = NA,
-                                           align = "right"),
-    Staff_Confirmed2    = clean_interpolate(Staff.Confirmed),
-    New_Staff_Cases = Staff_Confirmed2 - lag(Staff_Confirmed2),
-    New_Staff_Cases_7day = zoo::rollapply(data = Staff_Confirmed2 - lag(Staff_Confirmed2), 
-                                             width = 7,
-                                             FUN = mean,
-                                             na.rm = T,
-                                             fill = NA,
-                                             align = "right"),
-    New_Cases_All = New_Resident_Cases + New_Staff_Cases,
-    Staff_Recovered2 = clean_interpolate(Staff.Recovered),
-    Staff_Recovered_7day = zoo::rollapply(na.approx(Staff.Recovered, na.rm = F),
-                                              width = 7,
-                                              FUN = mean,
-                                              na.rm = T,
-                                              fill = NA,
-                                              align = "right"),
-    Staff_Active_7day = zoo::rollapply(Staff_Confirmed2 - Staff_Recovered2,
-                                           width = 7,
-                                           FUN = mean,
-                                           na.rm = T,
-                                           fill = NA,
-                                           align = "right"),
-    Staff_Deaths_7day = zoo::rollapply(na.approx(Staff.Deaths, na.rm = F),
-                                           width = 7,
-                                           FUN = mean,
-                                           na.rm = T,
-                                           fill = NA,
-                                           align = "right")
+    Resident_Outbreak_Day   = as.numeric(Date - Resident_Outbreak_Start),
+    Staff_Outbreak_Start    = min(Date[which(Staff.Confirmed > 0)], na.rm = T),
+    Staff_Outbreak_Day      = as.numeric(Date - Staff_Outbreak_Start),
+    Pop_interpolated        = na.approx(Capacity, na.rm = FALSE)
+  ) %>% 
+# Correct cumulative counts for days resulting in negative cumulative changes
+  mutate(
+    Residents_Confirmed2    = change_rmv_nas(Residents.Confirmed),
+    Staff_Confirmed2        = change_rmv_nas(Staff.Confirmed),
+    Residents_Recovered2    = change_rmv_nas(Residents.Recovered),
+    Staff_Recovered2        = change_rmv_nas(Staff.Recovered),
+    Residents_Deaths2       = change_rmv_nas(Residents.Deaths),
+    Staff_Deaths2           = change_rmv_nas(Staff.Deaths)
+  ) %>% 
+# Fill days that result in negatives with NA  
+  tidyr::fill(Residents_Confirmed2:Staff_Deaths2) %>% 
+  mutate(
+    New_Residents_Confirmed = Residents_Confirmed2 - lag(Residents_Confirmed2),
+    New_Staff_Confirmed     = Staff_Confirmed2 - lag(Staff_Confirmed2),
+    New_Residents_Recovered = Residents_Recovered2 - lag(Residents_Recovered2),
+    New_Staff_Recovered     = Staff_Recovered2 - lag(Staff_Recovered2),
+    New_Residents_Deaths    = Residents_Deaths2 - lag(Residents_Deaths2),
+    New_Staff_Deaths        = Staff_Deaths2 - lag(Staff_Deaths2)
+  )
+
+# Still a couple dozen days that end up with negative incident countsamong residents, but nothing that appears systematic and all values are small negative (-4 or greater), so will simply replace them with 0 and move on. Staff on the other hand still have days that result in very large negative numbers, particularly around labor day (09-05-2020) and the beginning of December, so best for now to stick to analyses that rely on Resident metrics only
+
+  #View(intermediate_clean %>% filter(New_Staff_Confirmed < 0))
+
+
+
+
+fin_dat <- intermediate_clean %>% 
+  mutate(
+    New_Residents_Confirmed_rmv_neg = if_else(New_Residents_Confirmed < 0, 0, New_Residents_Confirmed),
+    New_Residents_Recovered_rmv_neg = if_else(New_Residents_Recovered < 0, 0, New_Residents_Recovered),
+    New_Residents_Deaths_rmv_neg    = if_else(New_Residents_Deaths < 0, 0, New_Residents_Deaths),
+    New_Residents_Confirmed_7day    = zoo::rollapply(
+      data = New_Residents_Confirmed_rmv_neg,
+      width = 7,
+      FUN = mean,
+      na.rm = T,
+      fill = NA,
+      align = "right"
+    ),
+    New_Residents_Recovered_7day    = zoo::rollapply(
+      data = New_Residents_Recovered_rmv_neg,
+      width = 7,
+      FUN = mean,
+      na.rm = T,
+      fill = NA,
+      align = "right"
+    ),
+    New_Residents_Deaths_7day       = zoo::rollapply(
+      data = New_Residents_Deaths_rmv_neg,
+      width = 7,
+      FUN = mean,
+      na.rm = T,
+      fill = NA,
+      align = "right"
+    ),
+    Residents_Active                = cumsum_diffs(
+      New_Residents_Confirmed_rmv_neg,
+      New_Residents_Recovered_rmv_neg,
+      New_Residents_Deaths_rmv_neg
+    ),
+    Residents_Active_7day           = zoo::rollapply(
+      data = Residents_Active,
+      width = 7,
+      FUN = mean,
+      na.rm = T,
+      fill = NA,
+      align = "right"
+    ),
   ) %>% 
   dplyr::select("Facility", "Date", "Resident_Outbreak_Day", "Staff_Outbreak_Day", "Pop_interpolated", 
-                "Residents.Confirmed", "Residents_Confirmed2", "New_Resident_Cases", "New_Resident_Cases_7day",
-                "Residents.Recovered", "Residents_Recovered2", "Residents_Recovered_7day",
-                "Residents_Active_7day",
-                "Residents.Deaths", "Residents_Deaths_7day",
-                "Staff.Confirmed", "Staff_Confirmed2", "New_Staff_Cases", "New_Staff_Cases_7day", "New_Cases_All",
-                "Staff.Recovered", "Staff_Recovered2", "Staff_Recovered_7day",
-                "Staff_Active_7day",
-                "Staff.Deaths", "Staff_Deaths_7day",
+                "Residents.Confirmed", "Residents_Confirmed2", "New_Residents_Confirmed", "New_Residents_Confirmed_rmv_neg", "New_Residents_Confirmed_7day",
+                "Residents.Recovered", "Residents_Recovered2", "New_Residents_Recovered", "New_Residents_Recovered_rmv_neg", "New_Residents_Recovered_7day",
+                "Residents.Deaths", "Residents_Deaths2", "New_Residents_Deaths", "New_Residents_Deaths_rmv_neg", "New_Residents_Deaths_7day",
+                "Residents_Active", "Residents_Active_7day",
+                "Staff.Confirmed", "Staff_Confirmed2", "New_Staff_Confirmed", 
+                "Staff.Recovered", "Staff_Recovered2", "New_Staff_Recovered",
+                "Staff.Deaths", "Staff_Deaths2", "New_Staff_Deaths",
                 "Design_Capacity", "Percent_Occupied", "Staffed_Capacity", 
                 "Residents.Tadmin", "Staff.Tested", "Residents.Negative", "Staff.Negative", "Residents.Pending", "Staff.Pending", 
                 "Residents.Quarantine", "Staff.Quarantine", "Residents.Active", "Residents.Tested",
