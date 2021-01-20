969e0b66c4d7c5b2b327e047ec8852ead3fb035a
cmhoove14 <cmhoove14@gmail.com>
2021-01-19 21:07:17 -0800

    Add Rt estimation with staff as imported, residents as local cases

--- Analysis/2_5-Cori-Rt-Estimation.R
+++ Analysis/2_5-Cori-Rt-Estimation.R
@@ -35,8 +35,8 @@ cori_rt_res <- function(facility, dat){
   
   R_config <- EpiEstim::make_config(t_start = ts,
                                     t_end = te,
-                                    mean_si = 5.2, # From https://dx.doi.org/10.1016%2Fj.cegh.2020.08.007
-                                    std_si = 1,
+                                    mean_si = 5.2, # From https://dx.doi.org/10.2807%2F1560-7917.ES.2020.25.17.2000257
+                                    std_si = 1.72,
                                     seed = 430)
   
   Cori_R <- EpiEstim::estimate_R(incid = inc_dat,
@@ -58,3 +58,97 @@ cori_rts <- bind_rows(lapply(unique(dat$Facility),
 
 saveRDS(cori_rts, 
         here::here("data", "derived", "Cori_etal_Rts_residents.rds"))
+
+# Instantaneous Rt estimates using Cori et al method implemented in EpiEstim package assuming staff cases are imported ----------------------
+cori_rt_res_stf <- function(facility, dat){
+  
+  inc_dat <- dat %>% 
+    filter(Facility == facility) %>% 
+    ungroup() %>% 
+    mutate(New_Staff_Confirmed_rmv_neg = if_else(New_Staff_Confirmed < 0, 0, New_Staff_Confirmed)) %>% 
+    dplyr::select(Date, New_Residents_Confirmed_rmv_neg, New_Staff_Confirmed_rmv_neg) %>% 
+    rename(dates = Date,
+           local = New_Residents_Confirmed_rmv_neg,
+           imported = New_Staff_Confirmed_rmv_neg)
+  
+  # Restrict to beginning of outbreak
+  t_1st <- min(inc_dat$dates[which(inc_dat$imported != 0)])
+  inc_dat <- inc_dat %>% 
+    filter(dates >= t_1st)
+  
+  # Sliding weekly windows in which to estimate Rt
+  window = 7
+  ts <- window:(nrow(inc_dat)-window)
+  te <- window:(nrow(inc_dat)-window)+window
+  
+  R_config <- EpiEstim::make_config(t_start = ts,
+                                    t_end = te,
+                                    mean_si = 5.2, # From https://dx.doi.org/10.2807%2F1560-7917.ES.2020.25.17.2000257
+                                    std_si = 1.72,
+                                    seed = 430)
+  
+  Cori_R <- EpiEstim::estimate_R(incid = inc_dat,
+                                 method = "parametric_si",
+                                 config = R_config)
+  
+  out <- Cori_R$R %>% 
+    mutate(
+      Date = Cori_R$dates[c(window:(nrow(inc_dat)-window))],
+      Facility = facility
+    )
+  
+  return(out)
+  
+}
+
+res_stf_cori_rts <- bind_rows(lapply(unique(dat %>% 
+                                              filter(Resident_Outbreak_Day < Staff_Outbreak_Day) %>% 
+                                              pull(Facility)), 
+                             FUN = cori_rt_res_stf,  
+                             dat = dat))
+
+saveRDS(res_stf_cori_rts, 
+        here::here("data", "derived", "Cori_Rts_residents_staff.rds"))
+
+# Cohort Rt estimates (Wallinga and Teunis method) imlemented in EpiEstim package --------------------
+wt_rt_res <- function(facility, dat){
+  
+  inc_dat <- dat %>% 
+    filter(Facility == facility) %>% 
+    ungroup() %>% 
+    dplyr::select(Date, New_Residents_Confirmed_rmv_neg) %>% 
+    rename(dates = Date,
+           I = New_Residents_Confirmed_rmv_neg)
+  
+  # Restrict to beginning of outbreak
+  t_1st <- min(inc_dat$dates[which(inc_dat$I != 0)], na.rm = T)
+  inc_dat <- inc_dat %>% 
+    filter(dates >= t_1st)
+  
+  # Sliding weekly windows in which to estimate Rt
+  window = 7
+  ts <- window:(nrow(inc_dat)-window)
+  te <- window:(nrow(inc_dat)-window)+window
+  
+  R_config <- EpiEstim::make_config(t_start = ts,
+                                    t_end = te,
+                                    mean_si = 5.2, # From https://dx.doi.org/10.1016%2Fj.cegh.2020.08.007
+                                    std_si = 1.72,
+                                    seed = 430)
+  
+  WT_R <- EpiEstim::wallinga_teunis(incid = inc_dat,
+                                    method = "parametric_si",
+                                    config = R_config)
+  
+  out <- WT_R$R %>% 
+    mutate(
+      Date     = WT_R$dates[c(window:(nrow(inc_dat)-window))],
+      Facility = facility
+    )
+  
+  return(out)
+}
+
+wt_rts <- bind_rows(lapply(unique(dat$Facility), 
+                           FUN = wt_rt_res,  
+                           dat = dat))
--- Analysis/3-Plot-Rt.R
+++ Analysis/3-Plot-Rt.R
@@ -23,8 +23,32 @@ cori_rts_plot <- cori_rts_dat %>%
     labs(x = "Date",
          y = "Resident Active Cases")
 
+# Cori et al method assuming staff are imported and residents are local
+cori_rts_stf_res_dat <- readRDS(here::here("data", "derived", "Cori_Rts_residents_staff.rds"))
 
-# EpiNow2 Rt estimates
+cori_rts_stf_res_plot <- cori_rts_stf_res_dat %>% 
+  mutate(Facility2 = str_replace(Facility, " State Prison", "")) %>% 
+  ggplot() +
+    geom_line(aes(x = Date, y = `Median(R)`)) +
+    geom_ribbon(aes(x = Date, ymin = `Quantile.0.25(R)`, ymax = `Quantile.0.75(R)`),
+                col = "grey50", alpha = 0.5) +
+    facet_wrap(facets = "Facility2",
+               nrow = 5, ncol = 5,
+               labeller = label_wrap_gen()) +
+    theme_bw() +
+    theme(strip.text = element_text(size = 6.5)) +
+    labs(x = "Date",
+         y = expression(R[t]~estimate))
+
+ggsave(plot = cori_rts_stf_res_plot,
+       filename = here::here("Plots", "Cori_Rts_stf_imported_res_local.jpg"),
+       height = 6, 
+       width = 8,
+       units = "in",
+       dpi = 300)
+
+
+# EpiNow2 Rt estimates -----------------------
 Rts <- readRDS(here::here("data", "derived", "Rt_estimates_df.rds")) %>%   
   mutate(
     Facility = str_replace(region, " State Prison", "")
@@ -32,17 +56,18 @@ Rts <- readRDS(here::here("data", "derived", "Rt_estimates_df.rds")) %>%
 
 
 rt_plots <- Rts %>% 
-ggplot() +
-  geom_line(aes(x = date, y = median)) +
-  geom_ribbon(aes(ymax = upper_90, ymin = lower_90, x = date),
-              col = "grey50", alpha = 0.5) +
-  facet_wrap(facets = "Facility",
-             nrow = 4, ncol = 8,
-             labeller = label_wrap_gen()) +
-  theme_bw() +
-  theme(strip.text = element_text(size = 6)) +
-  labs(x = "Date",
-       y = expression(R[t]~Estimate))
+  ggplot() +
+    geom_line(aes(x = date, y = median)) +
+    geom_ribbon(aes(ymax = upper_90, ymin = lower_90, x = date),
+                col = "grey50", alpha = 0.5) +
+    geom_hline(yintercept = 1, lty = 3) +
+    facet_wrap(facets = "Facility",
+               nrow = 4, ncol = 8,
+               labeller = label_wrap_gen()) +
+    theme_bw() +
+    theme(strip.text = element_text(size = 6)) +
+    labs(x = "Date",
+         y = expression(R[t]~Estimate))
 
 ggsave(plot = rt_plots,
        filename = here::here("Plots", "Rt_by_date_faceted.jpg"),
