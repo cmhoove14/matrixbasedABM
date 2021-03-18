afab3d6ad9380432d7fddcf5cc988b18dab38535
cmhoove14 <cmhoove14@gmail.com>
2021-03-18 12:52:28 -0700

    Update for new state database

--- data/get/get_CA_cases_pops.R
+++ data/get/get_CA_cases_pops.R
@@ -1,34 +1,5 @@
 library(data.table)
 library(tidyverse)
-
-# Get county fips codes ~~~~~~~~~~~~~~~~~~~~~~~~~---------------------------
-temp.file <- paste(tempfile(),".xlsx",sep = "")
-download.file("https://www2.census.gov/programs-surveys/popest/geographies/2018/all-geocodes-v2018.xlsx", 
-              temp.file, mode = "wb")
-
-fips <- readxl::read_excel(temp.file, skip = 4)
-  ca_cnty_fips <- fips %>% 
-    filter(`State Code (FIPS)` == "06" &
-             `County Code (FIPS)` != "000") %>% 
-    mutate(County = `Area Name (including legal/statistical area description)`,
-           fips = paste0(`State Code (FIPS)`, `County Code (FIPS)`)) %>% 
-    dplyr::select(County, fips)
-  ca_cnty_fips_dt <- as.data.table(ca_cnty_fips)
-
-# Get county populations ~~~~~~~~~~~~~~~~~~~~~~~~~~~----------------------  
-temp.file2 <- paste(tempfile(),".xlsx", sep = "")
-download.file("https://www2.census.gov/programs-surveys/popest/tables/2010-2019/counties/totals/co-est2019-annres-06.xlsx", 
-              temp.file2, mode = "wb")
-
-pops <- readxl::read_excel(temp.file2, skip = 3)
-  pops <- pops[2:59,c(1, ncol(pops))]
-  colnames(pops) <- c("County", "pops")
-  
-  pops$County <- substr(pops$County, 2, nchar(pops$County)-(nchar(",California")+1))
-  
-  pops$County %in% ca_cnty_fips_dt$County
-  
-  ca_cnty_fips_dt <- merge(ca_cnty_fips_dt, pops, by = "County")
   
 # Get case data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~---------------------------
   system(paste0("curl -o ", here::here("data", "raw", "CA_cases"), Sys.Date(), ".csv " ,
